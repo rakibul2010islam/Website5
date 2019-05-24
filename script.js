5d04feb4eb107134015b3b2cc0a498a8d82f9d6e
Md. Rakibul Islam <rakibul2010islam@gmail.com>
2019-05-25 02:59:14 +0600

    nav-fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var navButton = $('.hamburgerButton > button');
 
 navButton.click(function(){
-	$('nav > ul > li').toggleClass('toggleNav');
-	$('nav > ul > li:first-child').toggleClass('li-hide');
+	$('nav > div > ul').toggleClass('toggleNav');
+	$('nav > div > ul > li').toggleClass('toggleNav');
 })
