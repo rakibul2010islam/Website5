8d8729c277e04e01485104f06a131ca4cbc05aa0
Md. Rakibul Islam <rakibul2010islam@gmail.com>
2019-05-26 02:36:50 +0600

    cover-photo

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ var navButton = $('.hamburgerButton > button');
 navButton.click(function(){
 	$('nav > div > ul').toggleClass('toggleNav');
 	$('nav > div > ul > li').toggleClass('toggleNav');
+	$('.photo').toggleClass('.photo-positioning');
 })
